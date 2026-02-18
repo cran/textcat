5616a65f54b61a959f4dfe8c01df89343962ecee
Kurt Hornik <Kurt.Hornik@R-project.org>
2026-02-18 10:03:14 +0000

    version 1.0-10

---
+++ inst/REFERENCES.R
@@ -0,0 +1,62 @@
+bibentry(bibtype = "Article",
+         key = "Hornik+Mair+Rauch:2013",
+         title = "The {textca}t Package for $n$-Gram Based Text Categorization in {R}",
+         volume = "52",
+         .issn = "1548-7660",
+         doi = "10.18637/jss.v052.i06",
+         number = "6",
+         journal = "Journal of Statistical Software",
+         .publisher = "Foundation for Open Access Statistics",
+         author = c(person(given = "Kurt",
+                           family = "Hornik",
+                           comment = c(ORCID = "0000-0003-4198-9911")),
+                    person(given = "Patrick",
+                           family = "Mair"),
+                    person(given = "Johannes",
+                           family = "Rauch"),
+                    person(given = "Wilhelm",
+                           family = "Geiger"),
+                    person(given = "Christian",
+                           family = "Buchta"),
+                    person(given = "Ingo",
+                           family = "Feinerer",
+                           comment = c(ORCID = "0000-0001-7656-8338"))),
+         year = "2013")
+
+## In the textcat JSS paper, we used
+##   \url{https://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.53.9367}
+##   \url{https://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.44.950}
+## Unfortunately, these URLs no longer work, see e.g.
+## <https://math.meta.stackexchange.com/questions/35421/what-happened-to-citeseerx-is-there-a-way-to-fix-those-links>
+## in particular the pointer to using
+## <https://archive.org/download/citeseerx-csx_citegraph.2017-03-31/citeseerx_checksums.tsv.gz>
+
+bibentry(bibtype = "InProceedings",
+         key = "Armstrong-Warwick+Thompson+Mckelvie:1994",
+         author = c(person(given = "Susan",
+                           family = "Armstrong-Warwick"),
+                    person(given = c("Henry", "S."),
+                           family = "Thompson"),
+                    person(given = "David",
+                           family = "McKelvie"),
+                    person(given = "Dominique",
+                           family = "Petitpierre")),
+         title = "Data in Your Language: The {ECI} Multilingual Corpus\n                  1",
+         booktitle = "Proceedings of the International Workshop on Sharable\n                  Natural Language Resources",
+         pages = "97--106",
+         year = "1994",
+         address = "Nara, Japan",
+         url = "https://citeseerx.ist.psu.edu/pdf/2a251a15c2e84ec2d9da129fb5cecb266dc85e48")
+
+bibentry(bibtype = "InProceedings",
+         key = "Cavnar+Trenkle:1994",
+         author = c(person(given = c("William", "B."),
+                           family = "Cavnar"),
+                    person(given = c("John", "M."),
+                           family = "Trenkle")),
+         title = "N-Gram-Based Text Categorization",
+         booktitle = "Proceedings of {SDAIR}-94, 3rd Annual Symposium on\n                  Document Analysis and Information Retrieval",
+         address = "Las Vegas, US",
+         pages = "161--175",
+         year = "1994",
+         url = "https://citeseerx.ist.psu.edu/pdf/49af572ef8f7ea89db06d5e7b66e9369c22d7607")
